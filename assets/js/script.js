c1ef27d427368ce12feb8b5bb865ed6e7824b68b
Abel <egbeleabel@yahoo.com>
2022-11-28 00:16:40 +0000

    adds function getPassword

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,11 +90,36 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var passwordLength = Number(prompt("How many characters do you want for your password? Note: it must be between 10 and 64"));
+  if (passwordLength.length >= 10 && passwordLength.length <= 64) {
+    var lowerCase = confirm ("Would you like lowercase letters for your password?");
+    var upperCase = confirm ("Would you like uppercase letters for your password?");
+    var numericChar = confirm ("Would you like numbers for your password?");
+    var specialChar = confirm ("Would you like special characters for your password?");
+    var passwordOptions = [lowerCase, upperCase, numericChar, specialChar];
+  return passwordOptions;
+  } else {
+    alert ("Your password length is not up to the required length. Please choose password lenght between 10 and 64!");
+  }
+
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  var randomPass = "";
+  for (var i = 0; i < passwordLength.length; i++) {
+    if (lowerCase === true) {
+      for (var x of lowerCasedCharacters) {
+        randomPass += lowerCasedCharacters.charAt(Math.floor(Nath.random() * lowerCasedCharacters.length));
+      }
+    } else if (upperCase === true) {
+      for (var x of upperCasedCharacters) {
+        randomPass += upperCasedCharacters.charAt(Math.floor(Nath.random() * upperCasedCharacters.length));
+      }
+    }
+  }
+
 
 }
 
